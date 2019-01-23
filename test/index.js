0d3d2dccb067eec82c974d720957f506c04a6b38
Egidijus Gegeckas <egidijus.gegeckas@gmail.com>
2019-01-23 13:24:11 +0100

    replaced object cache and nedb with flat-cache, which can be persisted

--- test/index.js
+++ test/index.js
@@ -7,67 +7,67 @@ const { currencylayerToken = null, fixerToken = null } = require('../lib/tokens.
 const mainConfig = Object.assign({}, require('../lib/config'));
 const todayDate = moment().format(mainConfig.date.format);
 
-describe.only(`Test with default config, ${todayDate}`, () => {
-  const config = {
-    api: {
-      currencylayer: {
-        access_key: currencylayerToken,
-      },
-      fixer: {
-        access_key: fixerToken,
-      },
-    },
-  };
-  const fr = new FiatRates(config);
+// const config = {
+//   currency: {
+//     base: 'EUR',
+//     symbols: ['EUR', 'USD', 'CAD', 'GBP', 'JPY']
+//   },
+//   api: {
+//     currencylayer: {
+//       access_key: currencylayerToken,
+//     },
+//     fixer: {
+//       access_key: fixerToken,
+//     },
+//   },
+// };
+// const fiatRates = new FiatRates(config);
+// const fiatRatesConfig = fiatRates.getConfig();
 
+describe(`Test with default config, ${todayDate}`, () => {
   it('should get latest with main config settings', async () => {
-    const frConfig = fr.getConfig();
-    const data = await fr.get();
-    expect(data).to.have.property('base', frConfig.currency.base);
+    const data = await fiatRates.get();
+    expect(data).to.have.property('base', fiatRatesConfig.currency.base);
     expect(data).to.have.property('rates');
-    expect(data.rates).to.have.keys(frConfig.currency.symbols);
+    expect(data.rates).to.have.keys(fiatRatesConfig.currency.symbols);
     expect(data).to.have.property('requestDate', todayDate);
   });
 
   it('should return from cache for second time', async () => {
-    const data = await fr.get();
+    const data = await fiatRates.get();
+    expect(data).to.have.property('base', fiatRatesConfig.currency.base);
+    expect(data).to.have.property('rates');
+    expect(data.rates).to.have.keys(fiatRatesConfig.currency.symbols);
+    expect(data).to.have.property('requestDate', todayDate);
     expect(data).to.have.property('source', 'cache');
   });
-
-  // it('should return from db for new instance and from cache for old instance', async () => {
-  //   const fr2 = new FiatRates(config);
-  //   const data = await fr.get();
-  //   expect(data).to.have.property('source', 'cache');
-  //   const data2 = await fr2.get();
-  //   expect(data2).to.have.property('source', 'db');
-  // });
 });
 
-describe.only(`Test with custom config, ${todayDate}`, () => {
-  const config = {
-    currency: {
-      base: 'EUR',
-      symbols: ['EUR', 'USD', 'CAD', 'GBP', 'JPY']
-    },
-    api: {
-      currencylayer: {
-        access_key: currencylayerToken,
-      },
-      fixer: {
-        access_key: fixerToken,
-      },
-    },
-  };
-  const fr = new FiatRates(config);
+describe(`Test with custom config, ${todayDate}`, () => {
+  // const config = {
+  //   currency: {
+  //     base: 'EUR',
+  //     symbols: ['EUR', 'USD', 'CAD', 'GBP', 'JPY']
+  //   },
+  //   api: {
+  //     currencylayer: {
+  //       access_key: currencylayerToken,
+  //     },
+  //     fixer: {
+  //       access_key: fixerToken,
+  //     },
+  //   },
+  // };
+  // const fr = new FiatRates(config);
+  // console.log(fr.getConfig());
 
   it('should get latest with custom config', async () => {
-    const data = await fr.get();
+    const data = await fiatRates.get();
     expect(data).to.have.property('base', config.currency.base);
     expect(data).to.have.property('rates');
     expect(data.rates).to.have.keys(config.currency.symbols);
     expect(data).to.have.property('requestDate', todayDate);
   });
-
 });
 
 // describe(`Test get() with custom config, ${todayDate}`, () => {
@@ -175,50 +175,73 @@ describe.only(`Test with custom config, ${todayDate}`, () => {
 //   });
 // });
 
-// describe(`FOR TESTING, ${todayDate}`, () => {
-//   const config = {
-//     currency: {
-//       base: 'USD',
-//       symbols: ['EUR', 'USD', 'GBP', 'AUD'],
-//       valuesToFixed: false,
-//     },
-//     localStorage: {
-//       enabled: true,
-//     },
-//     primaryApi: 'exchangeratesapi',
-//     secondaryApi: 'fixer',
-//   };
-//   const fr = new FiatRates(config);
-//
-//   it('should get latest with base as Eur', async () => {
-//     // const data = await fr.get({baseValue: 10, date: '2019-01-17'});
-//     // console.log(fr.getConfig());
-//     const data = await fr.get({ baseValue: 10 });
-//     console.log('---------------');
-//     console.log(data);
-//     // expect(data).to.have.property('rates');
-//     // expect(data).to.have.property('base', config.currency.base);
-//     // expect(data.rates).to.have.keys(config.currency.symbols);
-//   });
-//
-//   // it('should get latest with base as USD', async () => {
-//   //   const data = await fr.get({baseValue: 10, date: '2019-01-17'});
-//   //   expect(data).to.have.property('rates');
-//   //   expect(data).to.have.property('base', config.currency.base);
-//   //   expect(data.rates).to.have.keys(config.currency.symbols);
-//   // });
-//
-//   // it('should get get it from memory', async () => {
-//   //   const config = {
-//   //     currency: {
-//   //       base: 'USD',
-//   //       symbols: ['EUR', 'USD', 'GBP', 'AUD'],
-//   //     },
-//   //   };
-//   //   const fr = new FiatRates(config);
-//   //   const data = await fr.get();
-//   //   expect(data).to.have.property('rates');
-//   //   expect(data).to.have.property('base', config.currency.base);
-//   //   expect(data.rates).to.have.keys(config.currency.symbols);
-//   // });
-// });
+describe.only(`FOR TESTING, ${todayDate}`, () => {
+  const config = {
+    cache: {
+      enabled: true,
+      daysToPreload: 3,
+    },
+    currency: {
+      base: 'USD',
+      symbols: ['EUR', 'USD', 'GBP', 'AUD'],
+      valuesToFixed: false,
+    },
+    primaryApi: 'exchangeratesapi',
+    secondaryApi: null,
+  };
+  const fr = new FiatRates(config);
+
+  it('should get latest with base as Eur', async () => {
+    // const data = await fr.get({baseValue: 10, date: '2019-01-17'});
+    // console.log(fr.getConfig());
+    const data = await fr.get({ date: '2019-01-19', baseValue: 10 });
+    console.log('---------------');
+    console.log(data);
+    // expect(data).to.have.property('rates');
+    // expect(data).to.have.property('base', config.currency.base);
+    // expect(data.rates).to.have.keys(config.currency.symbols);
+  });
+
+  it('should get latest with base as Eur', async () => {
+    // const data = await fr.get({baseValue: 10, date: '2019-01-17'});
+    // console.log(fr.getConfig());
+    const data = await fr.get({ date: '2019-01-20', baseValue: 20 });
+    console.log('---------------');
+    console.log(data);
+    // expect(data).to.have.property('rates');
+    // expect(data).to.have.property('base', config.currency.base);
+    // expect(data.rates).to.have.keys(config.currency.symbols);
+  });
+
+  it('should get latest with base as Eur', async () => {
+    // const data = await fr.get({baseValue: 10, date: '2019-01-17'});
+    // console.log(fr.getConfig());
+    const data = await fr.get({ date: '2019-01-20', baseValue: 30, symbols: ['CAD', 'PHP'] });
+    console.log('---------------');
+    console.log(data);
+    // expect(data).to.have.property('rates');
+    // expect(data).to.have.property('base', config.currency.base);
+    // expect(data.rates).to.have.keys(config.currency.symbols);
+  });
+
+  // it('should get latest with base as USD', async () => {
+  //   const data = await fr.get({baseValue: 10, date: '2019-01-17'});
+  //   expect(data).to.have.property('rates');
+  //   expect(data).to.have.property('base', config.currency.base);
+  //   expect(data.rates).to.have.keys(config.currency.symbols);
+  // });
+
+  // it('should get get it from memory', async () => {
+  //   const config = {
+  //     currency: {
+  //       base: 'USD',
+  //       symbols: ['EUR', 'USD', 'GBP', 'AUD'],
+  //     },
+  //   };
+  //   const fr = new FiatRates(config);
+  //   const data = await fr.get();
+  //   expect(data).to.have.property('rates');
+  //   expect(data).to.have.property('base', config.currency.base);
+  //   expect(data.rates).to.have.keys(config.currency.symbols);
+  // });
+});
